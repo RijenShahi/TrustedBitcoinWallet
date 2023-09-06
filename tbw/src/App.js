209d54242c1db5021b2e94ffe5e30c6205e27485
RijenShahi <50471796+RijenShahi@users.noreply.github.com>
2023-09-06 19:14:28 +0545

    initaliation

---
+++ tbw/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Trusted Bitcoin Wallet</header>
+    </div>
+  );
+}
+
+export default App;
